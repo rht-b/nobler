a8fe9a067a5fbae0ac6064f86a07200d71a42a80
Rohit Bhattar <rhtbhattar@gmail.com>
2021-10-14 03:20:01 -0400

    fix related to cache at server

--- src/Persistent.cpp
+++ src/Persistent.cpp
@@ -1,6 +1,7 @@
-#include "Persistent.h"
+#include "../inc/Persistent.h"
 #include "Data_Transfer.h"
 #include <cassert>
+#include "Data_Server.h"
 
 Persistent::Persistent(const std::string& directory){
     
@@ -8,13 +9,18 @@ Persistent::Persistent(const std::string& directory){
     // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
     options.IncreaseParallelism();
     options.OptimizeLevelStyleCompaction();
+//    options.merge_operator.reset(new Persistent_merge);
     options.create_if_missing = true;
     rocksdb::Status status = rocksdb::DB::Open(options, directory, &db);
     assert(status.ok());
 }
 
+Persistent::~Persistent(){
+    delete db;
+}
+
 //TODO:: Get the interface uniform with Cache interface
-const std::vector<std::string> Persistent::get(const std::string& key){
+std::vector<std::string> Persistent::get(const std::string& key){
     std::string value;
     rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &value);
     
@@ -23,18 +29,18 @@ const std::vector<std::string> Persistent::get(const std::string& key){
         return std::vector<std::string>();
     }
     else{
-        DPRINTF(DEBUG_CAS_Server, "persisttent get VALUE FOUND! key is %s :  and the value is %s\n", key.c_str(),
-                value.c_str());
-        std::vector <std::string> raw_data = DataTransfer::deserialize(value);
-        DPRINTF(DEBUG_CAS_Server, "PERSISTENT GET~!!! -- size after decoding : %lu\n", raw_data[0].size());
-        return raw_data;
+        DPRINTF(DEBUG_CAS_Server, "persisttent get VALUE FOUND! key is %s :  and the value size is %zu\n", key.c_str(),
+                value.size());
+        return DataTransfer::deserialize(value);
+//        DPRINTF(DEBUG_CAS_Server, "PERSISTENT GET~!!! -- size after decoding : %lu\n", raw_data[0].size());
+//        return raw_data;
     }
 }
 
-void Persistent::put(const std::string& key, std::vector <std::string> value){
+void Persistent::put(const std::string& key, const std::vector<std::string>& value){
     
     if(value.empty()){
-        std::cout << "Error ! emtpy insertion" << std::endl;
+        DPRINTF(DEBUG_CAS_Server, "Error: emtpy insertion\n");
         assert(0);
     }
     std::string out_str = DataTransfer::serialize(value);
@@ -49,58 +55,69 @@ void Persistent::put(const std::string& key, std::vector <std::string> value){
     
     rocksdb::Status s = db->Put(rocksdb::WriteOptions(), key, out_str);
     if(!s.ok()){
-        std::cerr << s.ToString() << std::endl;
+        DPRINTF(DEBUG_CAS_Server, "db->Put Error: %s\n", s.ToString().c_str());
+//        std::cerr << s.ToString() << std::endl;
         assert(s.ok());
     }
 }
 
-bool Persistent::exists(const std::string& key){
-    
-    std::string _value;
-    rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &_value);
-    if(s.IsNotFound()){
-        return false;
+void Persistent::merge(const std::string& key, const std::vector<std::string>& value){
+    if(value.empty()){
+        DPRINTF(DEBUG_CAS_Server, "Error: emtpy insertion\n");
+        assert(0);
     }
-    else{
-        return true;
-    }
-}
-
-void Persistent::modify_flag(const std::string& key, int label){
-    
-    std::string _value;
-    rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &_value);
-    assert(s.ok());
-    
-    //Modify the First Byte
-    std::vector <std::string> raw_data = DataTransfer::deserialize(_value);
-    raw_data[1] = static_cast<char>(label);
-    _value = DataTransfer::serialize(raw_data);
-    
-    s = db->Put(rocksdb::WriteOptions(), key, _value);
+    std::string out_str = DataTransfer::serialize(value);
+    rocksdb::Status s = db->Merge(rocksdb::WriteOptions(), key, out_str);
     if(!s.ok()){
-        std::cerr << s.ToString() << std::endl;
+        DPRINTF(DEBUG_CAS_Server, "db->Put Error: %s\n", s.ToString().c_str());
+//        std::cerr << s.ToString() << std::endl;
         assert(s.ok());
     }
-    
 }
+
+//bool Persistent::exists(const std::string& key){
+//
+//    std::string _value;
+//    rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &_value);
+//    if(s.IsNotFound()){
+//        return false;
+//    }
+//    else{
+//        return true;
+//    }
+//}
+
+//void Persistent::modify_flag(const std::string& key, int label){
+//
+//    std::string _value;
+//    rocksdb::Status s = db->Get(rocksdb::ReadOptions(), key, &_value);
+//    assert(s.ok());
+//
+//    //Modify the First Byte
+//    std::vector <std::string> raw_data = DataTransfer::deserialize(_value);
+//    raw_data[1] = static_cast<char>(label);
+//    _value = DataTransfer::serialize(raw_data);
+//
+//    s = db->Put(rocksdb::WriteOptions(), key, _value);
+//    if(!s.ok()){
+//        std::cerr << s.ToString() << std::endl;
+//        assert(s.ok());
+//    }
+//
+//}
 /*
 // First Byte in the byte stream is actually the "Fin" tag
 // So the value field starts from the second byte
 std::string& Persistent::encode(std::vector<std::string> &value){
-
 	if(value.size() > 1){
 		value[0].insert(0,value[1]);
 		std::cout<<"ENCODE:  Value actaully inserted is " << value[0] <<std::endl;
 	}
 	return value[0];
 }
-
 std::vector<std::string> Persistent::decode(const std::string &value){
-
 	 std::vector<std::string> _value;
 	_value.push_back(std::string(value.begin()+1, value.end()) );
 	_value.push_back(std::string(value,0,1) );
-
 	return _value;
 }*/
